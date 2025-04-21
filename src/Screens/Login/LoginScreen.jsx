881979856cb241623a4a5b1dfca3ad66666c3a8e
Daniela Ramírez de León <134820840+dannyrmrz@users.noreply.github.com>
2025-04-20 23:12:09 -0600

    Casilla de contraseña
    Casilla de contraseña, ícono para ver y ocultar contraseña, toggle Password Visibility

--- src/Screens/Login/LoginScreen.jsx
+++ src/Screens/Login/LoginScreen.jsx
@@ -1,14 +1,36 @@
+/*
+LoginScreen
+Contenedores de la pantalla de inicio de sesión
+Contiene el input de nombre de usuario y su respectivo estado
+Contiene el input de contraseña y su respectivo estado
+Contiene el icono de mostrar/ocultar contraseña y su respectivo estado
+Autor: Daniela 
+Ultima modificación: 20/04/2025
+*/
+
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 const LoginScreen = () => {
     const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleInputChange = (e) => {
         setUsername(e.target.value);
     };
 
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
     return (
-        <div style={{ padding: '20px' }}>
+        <div style={{ padding: '20px' }}> {/* Casilla de Usuario*/}
             <input
                 type="text"
                 id="username"
@@ -23,6 +45,36 @@ const LoginScreen = () => {
                     borderRadius: '4px',
                 }}
             />
+            {/* Casilla de contraseña */}
+            <div style={{ position: 'relative', maxWidth: '300px' }}> 
+                <input
+                    type={showPassword ? 'text' : 'password'}
+                    id="password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                    placeholder="Contraseña"
+                    style={{
+                        padding: '8px',
+                        width: '100%',
+                        border: '1px solid #ccc',
+                        borderRadius: '4px',
+                    }}
+                />
+                <span
+                    onClick={togglePasswordVisibility} //funcion para mostrar/ocultar contraseña
+                    style={{
+                        position: 'absolute',
+                        right: '10px',
+                        top: '50%',
+                        transform: 'translateY(-50%)',
+                        cursor: 'pointer',
+                        userSelect: 'none',
+                    }}
+                >
+                    {/* Icono de mostrar/ocultar contraseña */}
+                    <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} /> 
+                </span>
+            </div>
         </div>
     );
 };
