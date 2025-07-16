a46e9f43771db80369880565d0f6eb22fa84c8e6
Renato Rojas <roj23813@uvg.edu.gt>
2025-07-16 14:57:33 -0600

    testing agregado (authService.test)

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.js',
+    css: true,
+    transformMode: {
+      web: [/\.jsx$/],
+    },
+    include: ['src/**/*.test.{js,ts,jsx,tsx}'],
+    watch:false,
+  },
   server: {
     port: 5173,
     host:true
